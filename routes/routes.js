ec8f2fbe6672624ac1eb336785da45f28eb3649a
Lukaskov <sabin6341@gmail.com>
2019-02-20 17:48:01 +0200

    [ft-#164111544]  updates routes

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 
 import partyController from "../controller/partycontroller";
-//import officesController from "../controller/officecontroller";
+import officeController from "../controller/officecontroller";
 import usersController from "../controller/usercontroller";
 //import candidatesController from "../controller/candidatecontroller";
 //import petitionsController from "../controller/petitioncontroller";
@@ -12,7 +12,7 @@ import usersController from "../controller/usercontroller";
 //post routers
 router.get("/api/v1/parties", partyController.AllParty);
 //router.get("/api/v1/user", usersController.getuser);
-//router.get("/api/v1/office", officesController.getoffice);
+router.get("/api/v1/office", officeController.Alloffice);
 //router.get("/api/v1/candidates",candidatesController.getcandidates);
 //router.get("/api/v1/votes", voteController.getvotes);
 //router.get("/api/v1/petition", petitionsController.getpetition);
@@ -22,20 +22,20 @@ router.post("/api/v1/parties", partyController.createParty);
 //router.post("/api/v1/user", usersController.createuser);
 //router.post("/api/v1/candidates/",candidatesController.createcandidates);
 //router.post("/api/v1/petition/", petitionsController.createpetition);
-//router.post("/api/v1/office/", officesController.createoffice);
+router.post("/api/v1/office/", officeController.createoffice);
 //router.post("/api/v1/votes/", voteController. createvote);
 
 //get by id  routers
 //router.get("/api/v1/user/:id", usersController.getOneuser);
 router.get("/api/v1/parties/:id", partyController.GetPartById);
-//router.get("/api/v1/office/:id", officesController.getOneoffice);
+router.get("/api/v1/office/:id", officeController.GetofficeById);
 //router.get("/api/v1/candidates/:id", candidatesController.getOnecandidate);
 //router.get("/api/v1/votes/:id", voteController.getOnevote);
 //router.get("/api/v1/petition/:id",petitionsController.getOnepetition);
 
 //updates routers
 router.put("/api/v1/parties/:id",partyController.updateparty);
-//router.put("/api/v1/office/:id",officesController.updateoffice);
+router.put("/api/v1/office/:id",officeController.updateoffice);
 //router.put("/api/v1/user/:id", usersController.updateuser);
 
 
@@ -43,10 +43,10 @@ router.put("/api/v1/parties/:id",partyController.updateparty);
 //router.patch("/api/v1/parties/:id/:name", partyController.updatepartyname);
 
 //delete routers
-//router.delete("/api/v1/parties/:id", partyController.deleteparties);
+router.delete("/api/v1/parties/:id", partyController.deleteparty);
 //router.delete("/api/v1/user/:id", usersController.deleteuser);
 //router.delete("/api/v1/candidates/:id", candidatesController.deletecandidate);
-//router.delete("/api/v1/office/:id",officesController.deleteoffice);
+router.delete("/api/v1/office/:id",officeController.deleteoffice);
 //router.delete("/api/v1/petition/:id", petitionsController.deletepetition);
 
 // accept the data from users signing up
